ed5b968acfce4034c25cbbce4551a307343e6cbd
Paulojoserc <pjrc83@gmail.com>
2022-11-20 22:58:56 -0300

    Classe conta concluída

--- src/poo/introducao/desafio/contabancaria/ContaBancaria.java
+++ src/poo/introducao/desafio/contabancaria/ContaBancaria.java
@@ -1,54 +1,88 @@
 package poo.introducao.desafio.contabancaria;
 
+import java.util.Date;
+
 public class ContaBancaria {
-	private String nomeCliente;
-	private String numConta;
-	private double saldo;
+	Long codigo;
+	String nomeCliente;
+	Date dataDeNascimento;
+	double saldo;
 
-	public String getNomeCliente() {
-		return nomeCliente;
+	{
+		this.codigo = (long) (Math.random() * 1000) + 10;
 	}
 
-	public void setNomeCliente(String nomeCliente) {
+	public ContaBancaria() {
+	}
+
+	public ContaBancaria(Long codigo, String nomeCliente, Date dataDeNascimento, double saldo) {
+		super();
+		this.codigo = codigo;
 		this.nomeCliente = nomeCliente;
-	}
-
-	public String getNumConta() {
-		return numConta;
-	}
-
-	public void setNumConta(String numConta) {
-		this.numConta = numConta;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public void setSaldo(double saldo) {
+		this.dataDeNascimento = dataDeNascimento;
 		this.saldo = saldo;
 	}
 
+	public void transferencia(ContaBancaria destino, double valor) {
+		System.out.println();
+		System.out.println(" Realizando saque ");
+		System.out.println("Seu saldo anteior é: "+this.saldo);
+		System.out.println("Saldo anteior Destino é: "+destino.saldo);
+		if ((this.saldo - valor) >= 0) {
+			this.saldo -= valor;
+			destino.saldo += valor;
+			System.out.println(" Saldo Posterior: "+this.saldo);
+			System.out.println(" Saldo Posterior: "+destino.saldo);
+		}else {
+			System.out.println(" Desculpe saldo insuficiente");
+		}
+		System.out.println(" Fim da Transferência");
+		System.out.println();
+		
+	}
+	
+	public void depositar(double valor) {
+		System.out.println();
+		System.out.println(" Realizando depósito ");
+		System.out.println("Seu saldo anteior é: "+this.saldo);
+		this.saldo += valor;
+		System.out.println("Saldo posterior: "+this.saldo);
+		System.out.println("Fim  depósito");
+		System.out.println();
+	}
+
+	public void sacar(double valor) {
+		System.out.println();
+		System.out.println(" Realizando saque ");
+		System.out.println("Seu saldo anteior é: "+this.saldo);
+		if ((this.saldo - valor) >= 0) {
+			this.saldo -= valor;
+			System.out.println(" Saldo Posterior: "+this.saldo);
+		}else {
+			System.out.println(" Desculpe saldo insuficiente");
+		}
+		System.out.println(" Fim da operação Saque");
+		System.out.println();
+	}
+	
+	public void verSaldo() {
+		System.out.println();
+		System.out.println("          Dados Bancários");
+		System.out.println("Número da conta: "+ this.codigo);
+		System.out.println("Titular: "+this.nomeCliente);
+		System.out.println(" Saldo: "+this.saldo);
+		System.out.println("      Fim da consulta");
+	}
+	
 	@Override
 	public String toString() {
-		
-		String s ="ContaBancaria [ ";
-		s +="Nome do Cliente: "+nomeCliente;
-		s +="; Numero da conta: "+numConta;
-		s +="; Saldo: "+saldo;
-		s+=" ]";
+
+		String s = "ContaBancaria [ ";
+		s += "Nome do Cliente: " + nomeCliente;
+		s += "; Numero da conta: " + codigo;
+		s += "; Saldo: " + this.saldo;
+		s += " ]";
 		return s;
 	}
 
-	public void depositar(double valor) {
-		saldo += valor;
-	}
-
-	public boolean sacar(double valor) {
-		if ((saldo - valor) >= 0) {
-			saldo -= valor;
-			return true;
-		}
-		return false;
-	}
 }
