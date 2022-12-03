10eb67dd2f028e23bc47273808d3a6262cc03d34
Paulojoserc <pjrc83@gmail.com>
2022-12-03 12:36:23 -0300

    erro ao criar 2º conta

--- src/poo/introducao/desafio/contabancaria/ContaBancaria.java
+++ src/poo/introducao/desafio/contabancaria/ContaBancaria.java
@@ -5,11 +5,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ContaBancaria {
-	Long codigo;
-	String nomeCliente;
-	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	Date dataDeNascimento;
-	double saldo;
+	private Long codigo;
+	private String nomeCliente;
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	private Date dataDeNascimento;
+	private double saldo;
 
 	{
 		this.codigo = (long) (Math.random() * 1000) + 10;
@@ -17,13 +17,12 @@ public class ContaBancaria {
 
 	public ContaBancaria() {
 	}
+
 	public ContaBancaria(String nomeCliente, Date dataDeNascimento, double saldo) {
-		this.codigo = codigo;
 		this.nomeCliente = nomeCliente;
 		this.dataDeNascimento = dataDeNascimento;
 		this.saldo = saldo;
 	}
-
 
 	public ContaBancaria(Long codigo, String nomeCliente, Date dataDeNascimento, double saldo) {
 		this.codigo = codigo;
@@ -32,30 +31,62 @@ public class ContaBancaria {
 		this.saldo = saldo;
 	}
 
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(Date dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
 	public void transferencia(ContaBancaria destino, double valor) {
 		System.out.println();
 		System.out.println(" Realizando Transferencia ");
-		System.out.println("Seu saldo anteior conta de origem: "+this.saldo);
-		System.out.println("Saldo anteior da conta destino é: "+destino.saldo);
+		System.out.println("Seu saldo anteior conta de origem: " + this.saldo);
+		System.out.println("Saldo anteior da conta destino é: " + destino.saldo);
 		if ((this.saldo - valor) >= 0) {
 			this.saldo -= valor;
 			destino.saldo += valor;
-			System.out.println(" Saldo Posterior  conta de origem: "+this.saldo);
-			System.out.println(" Saldo Posterior da conta destino é: "+destino.saldo);
-		}else {
+			System.out.println(" Saldo Posterior  conta de origem: " + this.saldo);
+			System.out.println(" Saldo Posterior da conta destino é: " + destino.saldo);
+		} else {
 			System.out.println(" Desculpe saldo insuficiente");
 		}
 		System.out.println(" Fim da Transferência");
 		System.out.println();
-		
+
 	}
-	
+
 	public void depositar(double valor) {
 		System.out.println();
 		System.out.println(" Realizando depósito ");
-		System.out.println("Seu saldo anteior é: "+this.saldo);
+		System.out.println("Seu saldo anteior é: " + this.saldo);
 		this.saldo += valor;
-		System.out.println("Saldo posterior: "+this.saldo);
+		System.out.println("Saldo posterior: " + this.saldo);
 		System.out.println("Fim  depósito");
 		System.out.println();
 	}
@@ -63,36 +94,35 @@ public class ContaBancaria {
 	public void sacar(double valor) {
 		System.out.println();
 		System.out.println(" Realizando saque ");
-		System.out.println("Seu saldo anteior é: "+this.saldo);
+		System.out.println("Seu saldo anteior é: " + this.saldo);
 		if ((this.saldo - valor) >= 0) {
 			this.saldo -= valor;
-			System.out.println(" Saldo Posterior: "+this.saldo);
-		}else {
+			System.out.println(" Saldo Posterior: " + this.saldo);
+		} else {
 			System.out.println(" Desculpe saldo insuficiente");
 		}
 		System.out.println(" Fim da operação Saque");
 		System.out.println();
 	}
-	
+
 	public void saldoBancario() throws ParseException {
 		System.out.println();
 		System.out.println("          Dados Bancários");
-		System.out.println("Número da conta: "+ this.codigo);
-		System.out.println("Titular: "+this.nomeCliente);
-		System.out.println("Data de nascimento: "+sdf.format(dataDeNascimento));
-		System.out.println(" Saldo: "+this.saldo);
+		System.out.println("Número da conta: " + this.codigo);
+		System.out.println("Titular: " + this.nomeCliente);
+		System.out.println("Data de nascimento: " + sdf.format(dataDeNascimento));
+		System.out.println(" Saldo: " + this.saldo);
 		System.out.println("      Fim da consulta");
 	}
+
 	@Override
 	public String toString() {
-		String s= "Dados Bancaria : ";
-	s+=	" Número da conta:" + codigo;
-	s+= " Titular: " + nomeCliente; 
-	s+=" Data de nascimento: " + sdf.format(dataDeNascimento); 
-	s+=" Saldo: R$" +String.format("%.2f%n", saldo) ;
-		 return s;
+		String s = "Dados Bancaria : ";
+		s += " Número da conta: " + codigo + "\n";
+		s += " Titular: " + nomeCliente + "\n";
+		s += " Data de nascimento: " + sdf.format(dataDeNascimento) + "\n";
+		s += " Saldo: R$" + String.format("%.2f%n", saldo);
+		return s;
 	}
-	
-	
 
 }
--- src/poo/introducao/desafio/contabancaria/TesteContaBancaria.java
+++ src/poo/introducao/desafio/contabancaria/TesteContaBancaria.java
@@ -2,7 +2,6 @@ package poo.introducao.desafio.contabancaria;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -10,39 +9,39 @@ import java.util.Scanner;
 
 public class TesteContaBancaria {
 
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		String aux, dataDeNascimento;
+		int opcao;
+		double valor;
+		long numeroConta1, numeroConta2;
+		List<ContaBancaria> contas = new ArrayList<>();
+		ContaBancaria contaBancaria = null;
 
-public static void main(String[] args) throws ParseException {
-	Scanner sc = new Scanner(System.in);
-	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	String aux, dataDeNascimento;
-	int opcao;
-	double valor;
-	long numeroConta1, numeroConta2;
-	List<ContaBancaria> contas = new ArrayList<>();
-	ContaBancaria contaBancaria = null, contaBancaria2 = null ;
-	
-	do {
-		System.out.println();
-		System.out.println("Entre como uma das opções abaixo");
-		System.out.println("1) Criar conta");
-		System.out.println("2) Dados bancario");
-		System.out.println("3) Deposito");
-		System.out.println("4) Sacar");
-		System.out.println("5) Tranferencia");
-		System.out.println("6) Sair do sistema");
-		
-		System.out.println();
-		System.out.println("Digite uma opção: ");
-		opcao = sc.nextInt();
-		
-		switch (opcao) {
+		do {
+			System.out.println();
+			System.out.println("Entre como uma das opções abaixo");
+			System.out.println("1) Criar conta");
+			System.out.println("2) Dados bancario");
+			System.out.println("3) Deposito");
+			System.out.println("4) Sacar");
+			System.out.println("5) Tranferencia");
+			System.out.println("6) Sair do sistema");
+
+			System.out.println();
+			System.out.println("Digite uma opção: ");
+			opcao = sc.nextInt();
+
+			switch (opcao) {
 			case 1:
 				System.out.println("Abrindo nova conta.....");
 				System.out.println("Digite o nome do cliente");
-				sc.next();
+				sc.nextLine();
 				aux = sc.nextLine();
+
 				System.out.println("Digite a data de nascimento");
-				dataDeNascimento =  sc.nextLine();
+				dataDeNascimento = sc.nextLine();
 				Date date = sdf.parse(dataDeNascimento);
 				System.out.println("Digite o valor do deposito");
 				valor = sc.nextDouble();
@@ -50,56 +49,62 @@ public static void main(String[] args) throws ParseException {
 				System.out.println(contaBancaria.toString());
 				contas.add(contaBancaria);
 				break;
-			
-		
+
 			case 2:
-				contaBancaria.saldoBancario();
+				System.out.println("Digite o número da conta para consulta ");
+				numeroConta1 = sc.nextLong();
+				for (ContaBancaria contaexiste : contas) {
+					if (numeroConta1 != contaexiste.getCodigo()) {
+						System.out.println("Conta não existe");
+					} else {
+						contaBancaria.saldoBancario();
+					}
+				}
 				break;
-			
-		
+
 			case 3:
 				System.out.println("Digite o valor do deposito: ");
 				valor = sc.nextDouble();
 				contaBancaria.depositar(valor);
 				break;
-			
-			
+
 			case 4:
 				System.out.println("Digite o valor do sacar: ");
 				valor = sc.nextDouble();
 				contaBancaria.sacar(valor);
 				break;
-			
-			
+
 			case 5:
-				System.out.println("Digite o número da conta de origem: ");
-				numeroConta1 = sc.nextLong();
-				if (numeroConta1!= contaBancaria.codigo) {
-					System.out.println("Conta de origem não existe");
-				}else {
-					contaBancaria.codigo = numeroConta1;	
+				for (ContaBancaria contaexiste : contas) {
+					System.out.println("Digite o número da conta de origem: ");
+					numeroConta1 = sc.nextLong();
+					if (numeroConta1 != contaexiste.getCodigo()) {
+						System.out.println("Conta de origem não existe");
+					} else {
+						contaBancaria.setCodigo(numeroConta1);
+					}
+
+					System.out.println("Digite o número da conta de destino: ");
+					numeroConta2 = sc.nextLong();
+					if (numeroConta2 != contaexiste.getCodigo()) {
+						System.out.println("Conta de destino não existe");
+					} else {
+						contaBancaria.setCodigo(numeroConta2);
+					}
+					System.out.println("Digite o valor da transferência ");
+					valor = sc.nextDouble();
+					contaBancaria.transferencia(contaBancaria, valor);
 				}
-				
-				System.out.println("Digite o número da conta de destino: ");
-				numeroConta2 = sc.nextLong();
-				if (numeroConta2!= contaBancaria.codigo) {
-					System.out.println("Conta de destino não existe");
-				}else {
-					contaBancaria2.codigo = numeroConta2;	
-				}
-				
-				System.out.println("Digite o valor da transferência ");
-				valor = sc.nextDouble();
-				contaBancaria.transferencia(contaBancaria2, valor);
+
 				break;
-			
-				
-		default:
-			break;
-		}
-	}while(opcao!=0) ;
-	
-	sc.close();
-}
+
+			default:
+				System.out.println("Opção invalida");
+				break;
+			}
+		} while (opcao != 0);
+
+		sc.close();
+	}
 
 }
